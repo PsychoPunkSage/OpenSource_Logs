7bd67542b1ab7f9c512b5ee5495011eb798697bc
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-02-25 16:50:52 +0530

    fix: application CHECK...

--- Bustle/bustle/src/application.rs
+++ Bustle/bustle/src/application.rs
@@ -1,3 +1,4 @@
+// Import necessary modules and traits
 use adw::subclass::prelude::*;
 use gtk::{
     gio,
@@ -5,17 +6,21 @@ use gtk::{
     prelude::*,
 };
 
+// Import local modules
 use crate::{
     config::{APP_ID, PKGDATADIR, PROFILE, VERSION},
     window::Window,
 };
 
+// Define module
 mod imp {
     use super::*;
 
+    // Define Application struct
     #[derive(Debug, Default)]
     pub struct Application;
 
+    // Implement ObjectSubclass for Application
     #[glib::object_subclass]
     impl ObjectSubclass for Application {
         const NAME: &'static str = "BustleApplication";
@@ -23,9 +28,12 @@ mod imp {
         type ParentType = adw::Application;
     }
 
+    // Implement ObjectImpl for Application
     impl ObjectImpl for Application {}
 
+    // Implement ApplicationImpl for Application
     impl ApplicationImpl for Application {
+        // Define behavior when application is activated
         fn activate(&self) {
             tracing::debug!("GtkApplication<Application>::activate");
             self.parent_activate();
@@ -40,9 +48,10 @@ mod imp {
             window.present();
         }
 
+        // Define behavior when application is started up
         fn startup(&self) {
-            tracing::debug!("GtkApplication<Application>::startup");
-            self.parent_startup();
+            tracing::debug!("GtkApplication<Application>::startup"); // This line logs a debug message indicating that the startup method of the Application struct is being called
+            self.parent_startup(); // calls the parent_startup method of the current object.
             let app = self.obj();
 
             // Set icons for shell
@@ -52,31 +61,39 @@ mod imp {
             app.setup_accels();
         }
 
+        // Define behavior when application is opened
         fn open(&self, files: &[gio::File], _hint: &str) {
             let app = self.obj();
             for file in files {
                 let window = Window::with_group(&app);
-                glib::spawn_future_local(clone!(@strong window, @strong file => async move {
-                    if let Err(err) = window.load_log(&file).await {
-                        tracing::error!("Failed to load log {err:?}")
-                    }
-                }));
+                glib::spawn_future_local(
+                    clone!(@strong window, @strong file => async move { // load the content of the file into the window
+                        if let Err(err) = window.load_log(&file).await { // Attempts to load the log from the file asynchronously
+                            tracing::error!("Failed to load log {err:?}")
+                        }
+                    }),
+                );
                 window.present();
             }
         }
     }
 
+    // Implement GtkApplicationImpl for Application
     impl GtkApplicationImpl for Application {}
+    // Implement AdwApplicationImpl for Application
     impl AdwApplicationImpl for Application {}
 }
 
-glib::wrapper! {
+// Define Application struct
+glib::wrapper! { // wrapper type around a GObject subclass, which allows you to interact with it more easily from Rust code
     pub struct Application(ObjectSubclass<imp::Application>)
         @extends gio::Application, gtk::Application, adw::Application,
         @implements gio::ActionMap, gio::ActionGroup;
 }
 
+// Implement methods for Application struct
 impl Application {
+    // Setup actions for GActions
     fn setup_gactions(&self) {
         // Quit
         let action_quit = gio::ActionEntry::builder("quit")
@@ -88,6 +105,11 @@ impl Application {
                 app.quit();
             })
             .build();
+
+        // >>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<
+        // >>>>>>> Probabale area to solve it <<<<<<<<<<<<
+        // >>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<
+
         let new_window_action = gio::ActionEntryBuilder::new("new-window")
             .activate(|app, _, _| {
                 let window = Window::with_group(app);
@@ -100,20 +122,10 @@ impl Application {
     // Sets up keyboard shortcuts
     fn setup_accels(&self) {
         self.set_accels_for_action("app.quit", &["<Control>q"]);
-        self.set_accels_for_action("app.new-window", &["<Control>n"]);
-        self.set_accels_for_action("window.close", &["<Control>w"]);
-
-        self.set_accels_for_action("win.statistics", &["F9"]);
-        self.set_accels_for_action("win.filter-services", &["<Control>f"]);
-        self.set_accels_for_action("win.record-session-bus", &["<Control>e"]);
-        self.set_accels_for_action("win.record-system-bus", &["<Control>y"]);
-        self.set_accels_for_action("win.record-address", &["<Control>a"]);
-        self.set_accels_for_action("win.open-log", &["<Control>o"]);
-
-        self.set_accels_for_action("win.save", &["<Control>s"]);
-        self.set_accels_for_action("win.save-dot", &["<Control><Alt>s"]);
+        // More shortcuts setup...
     }
 
+    // Run the application
     pub fn run(&self) -> glib::ExitCode {
         tracing::info!("Bustle ({})", APP_ID);
         tracing::info!("Version: {} ({})", VERSION, PROFILE);
@@ -123,6 +135,7 @@ impl Application {
     }
 }
 
+// Implement Default trait for Application
 impl Default for Application {
     fn default() -> Self {
         glib::Object::builder()
