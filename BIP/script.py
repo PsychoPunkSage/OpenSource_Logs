1fe8454d7d1f80a356a3e5f75f72d656e2c218cf
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-04-03 22:57:31 +0530

    add::> read_txn: returns a list of tx_ids in mempool

---
+++ BIP/script.py
@@ -0,0 +1,16 @@
+import json
+import os
+
+def read_transactions():
+    txn_ids = []
+    mempool_dir = "mempool"
+    try:
+        for filename in os.listdir(mempool_dir):
+            with open(os.path.join(mempool_dir, filename), "r") as file:
+                txn_ids.append(filename[:-5])
+        return txn_ids
+    except Exception as e:
+        print("Error:", e)
+        return None
+
+print(read_transactions())
