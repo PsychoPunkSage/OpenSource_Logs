943ff1034f4a70df70902031cad0edd2421328e3
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-04-11 22:13:50 +0530

    add::> compute_merkle_root() fn added

--- BIP/src/blocks.py
+++ BIP/src/blocks.py
@@ -10,18 +10,18 @@ import hashlib
 def double_sha256(data):
     return hashlib.sha256(hashlib.sha256(data).digest()).digest()
 
-# def compute_merkle_root(txn_hashes):
-#     # if no. transactions is odd
-#     if len(txn_hashes) % 2 == 1:
-#         txn_hashes.append(txn_hashes[-1])
+def compute_merkle_root(txn_hashes):
+    # if no. transactions is odd
+    if len(txn_hashes) % 2 == 1:
+        txn_hashes.append(txn_hashes[-1])
 
-#     tree = [double_sha256(hash) for hash in txn_hashes]
+    tree = [double_sha256(hash) for hash in txn_hashes]
 
-#     while len(tree) > 1:
-#         pairs = [(tree[i], tree[i+1]) for i in range(0, len(tree), 2)]
-#         tree = [double_sha256(pair[0] + pair[1]) for pair in pairs]
+    while len(tree) > 1:
+        pairs = [(tree[i], tree[i+1]) for i in range(0, len(tree), 2)]
+        tree = [double_sha256(pair[0] + pair[1]) for pair in pairs]
 
-#     return tree[0]
+    return tree[0]
 
 # def extract_txn_hashes_from_folder(folder_path):
 #     txn_hashes = []
