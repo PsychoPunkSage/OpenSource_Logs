cd80002a165a0eed1e3c94dba0732d5c02511fc1
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-04-15 01:11:29 +0530

    fix::> p2pkh: verification ready

--- BIP/src/scripts/p2pkh.py
+++ BIP/src/scripts/p2pkh.py
@@ -1,21 +1,31 @@
 import os
 import json
+import hashlib
 
 
-def validate_p2pkh_txn(txnId):
-    file_path = os.path.join('mempool', f'{txnId}.json') # file path
+def validate_p2pkh_txn(scriptsig_asm, scriptsig, scriptpubkey_asm):
+    stack = []
+    signature = ""
+    pubkey = ""
 
-    if os.path.exists(file_path):
-        # Read the JSON data from the file
-        with open(file_path, 'r') as file:
-            txn_data = json.load(file) # JSON Object
-    
-    # scriptpubkey = txn_data["vin"]["prevout"]["scriptpubkey"]
-    for i in txn_data["vin"]:
-        scriptpubkey_asm = i["prevout"]["scriptpubkey_asm"].split(" ")
-        scriptsig = i["scriptsig"]
-        scriptsig_asm = i["scriptsig_asm"]
-        print("scriptpubkey_asm:  ", scriptsig_asm, "\n\nscriptsig:  ", scriptsig, "\n\nscriptpubkey_asm:  ", scriptpubkey_asm)
-        # print(scriptpubkey_asm)
+    stack.append(signature)
+    stack.append(pubkey)
 
-validate_p2pkh_txn("1ccd927e58ef5395ddef40eee347ded55d2e201034bc763bfb8a263d66b99e5e")
+    for i in scriptpubkey_asm:
+        if i == "OP_DUP":
+            stack.append(stack[-1])
+        if i == "OP_HASH160":
+            ripemd160_hash = hashlib.new('ripemd160', bytes.fromhex(stack[-1])).digest()
+            stack.pop(stack[-1])
+            stack.append(ripemd160_hash.hex())
+        if i == "OP_EQUALVERIFY":
+            if stack[-1] != stack[-2]:
+                return False
+            continue
+        if i == "OP_CHECKSIG":
+            pass
+
+        else:
+            stack.append(i)
+
+# validate_p2pkh_txn("1ccd927e58ef5395ddef40eee347ded55d2e201034bc763bfb8a263d66b99e5e")
