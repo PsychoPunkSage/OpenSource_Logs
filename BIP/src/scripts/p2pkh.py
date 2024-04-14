fa6a2a98257e4d6b79c5403f8c4fec524e8983d4
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-04-15 00:29:36 +0530

    fix::> p2pkh: fetching done

--- BIP/src/scripts/p2pkh.py
+++ BIP/src/scripts/p2pkh.py
@@ -13,6 +13,9 @@ def validate_p2pkh_txn(txnId):
     # scriptpubkey = txn_data["vin"]["prevout"]["scriptpubkey"]
     for i in txn_data["vin"]:
         scriptpubkey_asm = i["prevout"]["scriptpubkey_asm"].split(" ")
-        print(scriptpubkey_asm)
+        scriptsig = i["scriptsig"]
+        scriptsig_asm = i["scriptsig_asm"]
+        print("scriptpubkey_asm:  ", scriptsig_asm, "\n\nscriptsig:  ", scriptsig, "\n\nscriptpubkey_asm:  ", scriptpubkey_asm)
+        # print(scriptpubkey_asm)
 
-validate_p2pkh_txn("0a3c3139b32f021a35ac9a7bef4d59d4abba9ee0160910ac94b4bcefb294f196")
+validate_p2pkh_txn("1ccd927e58ef5395ddef40eee347ded55d2e201034bc763bfb8a263d66b99e5e")
