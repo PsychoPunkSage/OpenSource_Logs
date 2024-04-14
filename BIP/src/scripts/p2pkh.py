427cea98e9dd0890ae6069f0bc79ebbea5c47ce5
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-04-15 01:19:30 +0530

    fix::> p2pkh: verification ready - checks added

--- BIP/src/scripts/p2pkh.py
+++ BIP/src/scripts/p2pkh.py
@@ -11,17 +11,33 @@ def validate_p2pkh_txn(scriptsig_asm, scriptsig, scriptpubkey_asm):
     stack.append(signature)
     stack.append(pubkey)
 
+    print(stack)
+
     for i in scriptpubkey_asm:
         if i == "OP_DUP":
             stack.append(stack[-1])
+            print("===========")
+            print("OP_DUP")
+            print(stack)
+
         if i == "OP_HASH160":
+            print("===========")
+            print("OP_HASH160")
             ripemd160_hash = hashlib.new('ripemd160', bytes.fromhex(stack[-1])).digest()
             stack.pop(stack[-1])
+            print(stack)
             stack.append(ripemd160_hash.hex())
+            print(stack)
+
         if i == "OP_EQUALVERIFY":
+            print("===========")
+            print("OP_EQUALVERIFY")
             if stack[-1] != stack[-2]:
                 return False
-            continue
+            else:
+                stack.pop(stack[-1])
+                stack.pop(stack[-2])
+
         if i == "OP_CHECKSIG":
             pass
 
