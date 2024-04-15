b1235b107d8a0033300670e2ca6f3fd2f8695355
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-04-15 23:55:10 +0530

    fix::> p2wpkh checks + 1

--- BIP/src/scripts/p2wpkh.py
+++ BIP/src/scripts/p2wpkh.py
@@ -5,6 +5,7 @@ def validate_p2wpkh_txn(witness, wit_scriptpubkey_asm):
     print(wit_sig, wit_pubkey)
     scriptpubkey_asm = ["OP_DUP", "OP_HASH160", "OP_PUSHBYTES_20", "pkh", "OP_EQUALVERIFY", "OP_CHECKSIG"]
     scriptpubkey_asm[3] = wit_scriptpubkey_asm[-1]
+    print(scriptpubkey_asm)
     return p2pkh.validate_p2pkh_txn(wit_sig, wit_pubkey, scriptpubkey_asm)
 
 
