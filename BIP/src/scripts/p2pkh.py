f865e6cb3924665e5ead23bde4ab9275f95a5d8a
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-04-16 03:07:11 +0530

    fix::> p2pkh OP_CHECKSIG - testing in progress

--- BIP/src/scripts/p2pkh.py
+++ BIP/src/scripts/p2pkh.py
@@ -1,20 +1,105 @@
 import os
 import json
-import hashlib
 import ecdsa
-import binascii
+import hashlib
 
-def parse_signature(signature_hex):
-    signature = binascii.unhexlify(signature_hex)
-    der_signature = ecdsa.util.sigdecode_der(signature, curve=ecdsa.SECP256k1.curve)
+def create_raw_txn_hash(txn_id):
+    txn_hash = ""
 
-    R, S = der_signature
+    file_path = os.path.join("mempool", f"{txn_id}.json")
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as f:
+            data = json.load(f)
+            # Version
+            txn_hash += f"{_little_endian(data['version'], 4)}"
+            # Marker+flags (if any `vin` has empty scriptsig)
+            if any(i.get("scriptsig") == "" for i in data["vin"]):
+                txn_hash += "0001"
+            # No. of inputs:
+            txn_hash += f"{str(_to_compact_size(len(data['vin'])))}"
+            # Inputs
+            for iN in data["vin"]:
+                txn_hash += f"{bytes.fromhex(iN['txid'])[::-1].hex()}"
+                txn_hash += f"{_little_endian(iN['vout'], 4)}"
+                txn_hash += f"{_to_compact_size(len(iN['scriptsig'])//2)}" # FLAG@> maybe not divided by 2
+                txn_hash += f"{iN['scriptsig']}"
+                txn_hash += f"{_little_endian(iN['sequence'], 4)}"
 
-    R_hex = hex(R)
-    S_hex = hex(S)
-    print(f"R::> {R_hex}")
-    print(f"S::> {S_hex}")
-    return R_hex, S_hex
+            # No. of outputs
+            txn_hash += f"{str(_to_compact_size(len(data['vout'])))}"
+
+            # Outputs
+            for out in data["vout"]:
+                txn_hash += f"{_little_endian(out['value'], 8)}"
+                txn_hash += f"{_to_compact_size(len(out['scriptpubkey'])//2)}"
+                txn_hash += f"{out['scriptpubkey']}"
+
+            # witness
+            for i in data["vin"]:
+                if "witness" in i and i["witness"]:
+                    txn_hash += f"{_to_compact_size(len(i['witness']))}"
+                    for j in i["witness"]:
+                        txn_hash += f"{_to_compact_size(len(j) // 2)}"
+                        txn_hash += f"{j}"
+
+            # Locktime
+            txn_hash += f"{_little_endian(data['locktime'], 4)}"
+            # print(f"txn_hash: {txn_hash}")
+    return txn_hash
+
+def create_raw_txn_hash_wo_witness(txn_id):
+    txn_hash = ""
+
+    file_path = os.path.join("mempool", f"{txn_id}.json")
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as f:
+            data = json.load(f)
+            # print(f"data: {data}")
+            # Version
+            txn_hash += f"{_little_endian(data['version'], 4)}"
+            # No. of inputs:
+            txn_hash += f"{str(_to_compact_size(len(data['vin'])))}"
+            # Inputs
+            for iN in data["vin"]:
+                txn_hash += f"{bytes.fromhex(iN['txid'])[::-1].hex()}"
+                txn_hash += f"{_little_endian(iN['vout'], 4)}"
+                txn_hash += f"{_to_compact_size(len(iN['scriptsig'])//2)}" # FLAG@> maybe not divided by 2
+                txn_hash += f"{iN['scriptsig']}"
+                txn_hash += f"{_little_endian(iN['sequence'], 4)}"
+
+            # No. of outputs
+            txn_hash += f"{str(_to_compact_size(len(data['vout'])))}"
+
+            # Outputs
+            for out in data["vout"]:
+                txn_hash += f"{_little_endian(out['value'], 8)}"
+                txn_hash += f"{_to_compact_size(len(out['scriptpubkey'])//2)}"
+                txn_hash += f"{out['scriptpubkey']}"
+
+            # Locktime
+            txn_hash += f"{_little_endian(data['locktime'], 4)}"
+    return txn_hash
+
+def _to_compact_size(value):
+    if value < 0xfd:
+        return value.to_bytes(1, byteorder='little').hex()
+    elif value <= 0xffff:
+        return (0xfd).to_bytes(1, byteorder='little').hex() + value.to_bytes(2, byteorder='little').hex()
+    elif value <= 0xffffffff:
+        return (0xfe).to_bytes(1, byteorder='little').hex() + value.to_bytes(4, byteorder='little').hex()
+    else:
+        return (0xff).to_bytes(1, byteorder='little').hex() + value.to_bytes(8, byteorder='little').hex()
+
+def _little_endian(num, size):
+    return num.to_bytes(size, byteorder='little').hex()
+
+def verify_sig(signature, pubKey, msg_bytes):
+    message = msg_bytes
+    public_key = pubKey
+    sig = signature
+
+    vk = ecdsa.VerifyingKey.from_string(bytes.fromhex(public_key), curve=ecdsa.SECP256k1, hashfunc=hashlib.sha256) # the default is sha1
+    return vk.verify(bytes.fromhex(sig), message)
 
 
 def hash160(hex_input):
@@ -25,7 +110,7 @@ def hash160(hex_input):
 
     return hash_160.hexdigest()
 
-def validate_p2pkh_txn(signature, pubkey, scriptpubkey_asm):
+def validate_p2pkh_txn(signature, pubkey, scriptpubkey_asm, txn_data):
     stack = []
 
     stack.append(signature)
@@ -63,8 +148,7 @@ def validate_p2pkh_txn(signature, pubkey, scriptpubkey_asm):
         if i == "OP_CHECKSIG":
             print("===========")
             print("OP_CHECKSIG")
-            parse_signature(stack[0])
-            return True
+            return verify_sig(stack[0], stack[1], bytes.fromhex(txn_data))
 
         if i == "OP_PUSHBYTES_20":
             print("===========")
@@ -81,4 +165,5 @@ if os.path.exists(file_path):
 scriptsig_asm = txn_data["vin"][0]["scriptsig_asm"].split(" ")
 # scriptsig = txn_data["vin"][0]["scriptsig"]
 scriptpubkey_asm = txn_data["vin"][0]["prevout"]["scriptpubkey_asm"].split(" ")
-validate_p2pkh_txn(scriptsig_asm[1], scriptsig_asm[3], scriptpubkey_asm)
+print(validate_p2pkh_txn(scriptsig_asm[1], scriptsig_asm[3], scriptpubkey_asm, create_raw_txn_hash("0a8b21af1cfcc26774df1f513a72cd362a14f5a598ec39d915323078efb5a240")))
+
