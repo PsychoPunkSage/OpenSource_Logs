3f699405f33d32cae97fb0f5ef1ab8cfd2464400
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-04-17 23:54:52 +0530

    fix::> removed comments

--- BIP/src/validate_txn.py
+++ BIP/src/validate_txn.py
@@ -132,9 +132,6 @@ def get_txn_id(txn_id):
     txnId = hashlib.sha256(bytes.fromhex(reversed_bytes)).digest().hex() # last sha256
     return txnId
 
-# ff0717b6f0d2b2518cfb85eed7ccea44c3a3822e2a0ce6e753feecf68df94a7f
-# 0a3c3139b32f021a35ac9a7bef4d59d4abba9ee0160910ac94b4bcefb294f196
-# 0a8b21af1cfcc26774df1f513a72cd362a14f5a598ec39d915323078efb5a240
 
 def validate(txnId):
 
