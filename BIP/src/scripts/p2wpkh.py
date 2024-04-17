b2c127e5d5ab32337f9db1b6b18eadaef79306c4
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-04-17 22:36:43 +0530

    fix::> OPCHECKSIG Wrking

--- BIP/src/scripts/p2wpkh.py
+++ BIP/src/scripts/p2wpkh.py
@@ -16,58 +16,71 @@ def _to_compact_size(value):
 def _little_endian(num, size):
     return num.to_bytes(size, byteorder='little').hex()
 
-def segwit_txn_data(txn_id):
+def segwit_txn_data1(txn_id):
     file_path = os.path.join("mempool", f"{txn_id}.json")
     if os.path.exists(file_path):
         with open(file_path, 'r') as f:
             data = json.load(f)
-            # Version
+            ## Version
             ver = f"{_little_endian(data['version'], 4)}"
 
-            # HASH256 (txid + vout) || HASH256 (sequece)
+            ## (txid + vout)
             serialized_txid_vout = ""
-            serialized_sequese= ""
             for iN in data["vin"]:
                 serialized_txid_vout += f"{bytes.fromhex(iN['txid'])[::-1].hex()}"
                 serialized_txid_vout += f"{_little_endian(iN['vout'], 4)}"
-                serialized_sequese += f"{_little_endian(iN['sequence'], 4)}"
+            # HASH256 (txid + vout)
+            hash256_in = hashlib.sha256(hashlib.sha256(bytes.fromhex(serialized_txid_vout)).digest()).digest().hex()
             
+            ## (sequense)
+            serialized_sequense= ""
+            for iN in data["vin"]:
+                serialized_sequense += f"{_little_endian(iN['sequence'], 4)}"
+            ## HASH256 (sequense)
+            hash256_seq = hashlib.sha256(hashlib.sha256(bytes.fromhex(serialized_sequense)).digest()).digest().hex()
+            
+            ###############################################################################
+            # TXN Specific #
+            ## TXID and VOUT for the REQUIRED_input
+            ser_tx_vout_sp = f"{bytes.fromhex(data['vin'][1]['txid'])[::-1].hex()}{_little_endian(data['vin'][1]['vout'], 4)}"
+            print(ser_tx_vout_sp)
+            ## Scriptcode
+            pkh = f"{data['vin'][1]['prevout']['scriptpubkey'][4:]}" 
+            scriptcode = f"1976a914{pkh}88ac"
+            ## Input amount
+            in_amt = f"{_little_endian(data['vin'][1]['prevout']['value'], 8)}"
+            ## SEQUENCE for the REQUIRED_input
+            sequence_txn = f"{_little_endian(data['vin'][1]['sequence'], 4)}"
+            ###############################################################################
+
             # Outputs
             serialized_output= ""
             for out in data["vout"]:
                 serialized_output += f"{_little_endian(out['value'], 8)}"
                 serialized_output += f"{_to_compact_size(len(out['scriptpubkey'])//2)}"
                 serialized_output += f"{out['scriptpubkey']}"
-
-            ###############################################################################
-            # TXN Specific #
-            pkh = f"{data['vin'][0]['prevout']['scriptpubkey'][6:-4]}" 
-            scriptcode = f"1976a914{pkh}88ac"
-
-            in_amt = f"{_little_endian(data['vin'][0]['prevout']['value'], 8)}"
-
-            sequence_txn = f"{_little_endian(data['vin'][0]['sequence'], 4)}"
-            ###############################################################################
-
-            locktime = f"{_little_endian(data['locktime'], 4)}"
-            print(serialized_txid_vout)
-            print(serialized_sequese)
-            print(serialized_output)
-            print(sequence_txn)
-            hash256_stv = hashlib.sha256(hashlib.sha256(bytes.fromhex(serialized_txid_vout)).digest()).digest().hex()
-            hash256_seq = hashlib.sha256(hashlib.sha256(bytes.fromhex(serialized_sequese)).digest()).digest().hex()
+            ## HASH256 (output)
             hash256_out = hashlib.sha256(hashlib.sha256(bytes.fromhex(serialized_output)).digest()).digest().hex()
-            # serialized_txid_vout
+
+            ## locktime
+            locktime = f"{_little_endian(data['locktime'], 4)}"
 
 
-            print(f"hash256 (txid + vout)::> {hash256_stv}")
-            print(f"hash256 (sequesnce)  ::> {hash256_seq}")
-            print(f"hash256 (output)     ::> {hash256_out}")
+            # print(serialized_txid_vout)
+            # print(serialized_sequense)
+            # print(serialized_output)
+            # print(sequence_txn)
+            # # serialized_txid_vout
+
+
+            # print(f"hash256 11111 (txid + vout)::> {hash256_stv}")
+            # print(f"hash256 11111 (sequesnce)  ::> {hash256_seq}")
+            # print(f"hash256 11111 (output)     ::> {hash256_out}")
 
             # preimage = version + hash256(inputs) + hash256(sequences) + input + scriptcode + amount + sequence + hash256(outputs) + locktime
-            preimage = ver + hash256_stv + hash256_seq + serialized_txid_vout + scriptcode + in_amt + sequence_txn + hash256_out + locktime
+            preimage = ver + hash256_in + hash256_seq + ser_tx_vout_sp + scriptcode + in_amt + sequence_txn + hash256_out + locktime
             preimage += "01000000"
-            print(f"preimage ::> {preimage}")
+            print(f"preimage 11111 ::> {preimage}")
     return hashlib.sha256(bytes.fromhex(preimage)).digest().hex()
 
 
@@ -81,3 +94,9 @@ def validate_p2wpkh_txn(witness, wit_scriptpubkey_asm):
     return p2pkh.validate_p2pkh_txn(wit_sig, wit_pubkey, scriptpubkey_asm)
 
 
+print(segwit_txn_data1("1ccd927e58ef5395ddef40eee347ded55d2e201034bc763bfb8a263d66b99e5e"))
+
+"""
+02000000f81369411d3fba4eb8575cc858ead8a859ef74b94e160a036b8c1c5b023a6fae957879fdce4d8ab885e32ff307d54e75884da52522cc53d3c4fdb60edb69a098659a6eaf8d943ad2ff01ec8c79aaa7cb4f57002d49d9b8cf3c9a7974c5bd3608060000002cbc395e5c16b1204f1ced9c0d1699abf5abbbb6b2eee64425c55252131df6c4000000001976a9147db10cfe69dae5e67b85d7b59616056e68b3512288acf1a2010000000000fdffffff0f38c28e7d8b977cd40352d825270bd20bcef66ceac3317f2b2274d26f973f0f0000000001000000
+02000000f81369411d3fba4eb8575cc858ead8a859ef74b94e160a036b8c1c5b023a6fae957879fdce4d8ab885e32ff307d54e75884da52522cc53d3c4fdb60edb69a098659a6eaf8d943ad2ff01ec8c79aaa7cb4f57002d49d9b8cf3c9a7974c5bd3608060000002cbc395e5c16b1204f1ced9c0d1699abf5abbbb6b2eee64425c55252131df6c4000000001976a9146dee3ed7e9a03ad379f2f78d13138f9141c794ed88acf306020000000000fdffffff0f38c28e7d8b977cd40352d825270bd20bcef66ceac3317f2b2274d26f973f0f0000000001000000
+"""
