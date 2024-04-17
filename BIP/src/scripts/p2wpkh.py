59ebc0d087d1571f808b9d5b7e3b3d59e6e67598
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-04-17 23:28:37 +0530

    fix::> p2wpkh fully working

--- BIP/src/scripts/p2wpkh.py
+++ BIP/src/scripts/p2wpkh.py
@@ -1,7 +1,16 @@
-import hashlib
-import json
 import os
+import json
 import p2pkh
+import hashlib
+import coincurve
+
+import p2pkh
+
+def validate_signature(signature, message, publicKey):
+    b_sig = bytes.fromhex(signature)
+    b_msg = bytes.fromhex(message)
+    b_pub = bytes.fromhex(publicKey)
+    return coincurve.verify_signature(b_sig, b_msg, b_pub)
 
 def _to_compact_size(value):
     if value < 0xfd:
@@ -16,7 +25,7 @@ def _to_compact_size(value):
 def _little_endian(num, size):
     return num.to_bytes(size, byteorder='little').hex()
 
-def segwit_txn_data1(txn_id):
+def segwit_txn_data(txn_id):
     file_path = os.path.join("mempool", f"{txn_id}.json")
     if os.path.exists(file_path):
         with open(file_path, 'r') as f:
@@ -65,38 +74,32 @@ def segwit_txn_data1(txn_id):
             ## locktime
             locktime = f"{_little_endian(data['locktime'], 4)}"
 
-
-            # print(serialized_txid_vout)
-            # print(serialized_sequense)
-            # print(serialized_output)
-            # print(sequence_txn)
-            # # serialized_txid_vout
-
-
-            # print(f"hash256 11111 (txid + vout)::> {hash256_stv}")
-            # print(f"hash256 11111 (sequesnce)  ::> {hash256_seq}")
-            # print(f"hash256 11111 (output)     ::> {hash256_out}")
-
             # preimage = version + hash256(inputs) + hash256(sequences) + input + scriptcode + amount + sequence + hash256(outputs) + locktime
             preimage = ver + hash256_in + hash256_seq + ser_tx_vout_sp + scriptcode + in_amt + sequence_txn + hash256_out + locktime
-            preimage += "01000000"
-            print(f"preimage 11111 ::> {preimage}")
-    return hashlib.sha256(bytes.fromhex(preimage)).digest().hex()
+            # print(f"preimage 11111 ::> {preimage}")
+    # return hashlib.sha256(bytes.fromhex(preimage)).digest().hex()
+    return preimage
 
 
-def validate_p2wpkh_txn(witness, wit_scriptpubkey_asm):
+def validate_p2wpkh_txn(witness, wit_scriptpubkey_asm, txn_data):
     wit_sig, wit_pubkey = witness[0], witness[1]
-    print(wit_sig, wit_pubkey)
-    scriptpubkey_asm = ["OP_DUP", "OP_HASH160", "OP_PUSHBYTES_20", "pkh", "OP_EQUALVERIFY", "OP_CHECKSIG"]
-    scriptpubkey_asm[3] = wit_scriptpubkey_asm[-1]
-    print(wit_scriptpubkey_asm[-1])
-    print(scriptpubkey_asm)
-    return p2pkh.validate_p2pkh_txn(wit_sig, wit_pubkey, scriptpubkey_asm)
 
+    pkh = wit_scriptpubkey_asm.split(" ")[-1]
+    print(wit_sig, wit_pubkey, pkh)
+    print("\n")
+    print(txn_data)
+    scriptpubkey_asm = ["OP_DUP", "OP_HASH160", "OP_PUSHBYTES_20", pkh, "OP_EQUALVERIFY", "OP_CHECKSIG"]
 
-print(segwit_txn_data1("1ccd927e58ef5395ddef40eee347ded55d2e201034bc763bfb8a263d66b99e5e"))
+    return p2pkh.validate_p2pkh_txn(wit_sig, wit_pubkey,scriptpubkey_asm, txn_data)
 
-"""
-02000000f81369411d3fba4eb8575cc858ead8a859ef74b94e160a036b8c1c5b023a6fae957879fdce4d8ab885e32ff307d54e75884da52522cc53d3c4fdb60edb69a098659a6eaf8d943ad2ff01ec8c79aaa7cb4f57002d49d9b8cf3c9a7974c5bd3608060000002cbc395e5c16b1204f1ced9c0d1699abf5abbbb6b2eee64425c55252131df6c4000000001976a9147db10cfe69dae5e67b85d7b59616056e68b3512288acf1a2010000000000fdffffff0f38c28e7d8b977cd40352d825270bd20bcef66ceac3317f2b2274d26f973f0f0000000001000000
-02000000f81369411d3fba4eb8575cc858ead8a859ef74b94e160a036b8c1c5b023a6fae957879fdce4d8ab885e32ff307d54e75884da52522cc53d3c4fdb60edb69a098659a6eaf8d943ad2ff01ec8c79aaa7cb4f57002d49d9b8cf3c9a7974c5bd3608060000002cbc395e5c16b1204f1ced9c0d1699abf5abbbb6b2eee64425c55252131df6c4000000001976a9146dee3ed7e9a03ad379f2f78d13138f9141c794ed88acf306020000000000fdffffff0f38c28e7d8b977cd40352d825270bd20bcef66ceac3317f2b2274d26f973f0f0000000001000000
-"""
+filename = "1ccd927e58ef5395ddef40eee347ded55d2e201034bc763bfb8a263d66b99e5e"
+file_path = os.path.join('mempool', f"{filename}.json") # file path
+if os.path.exists(file_path):
+    with open(file_path, 'r') as file: 
+        txn_data = json.load(file)
+
+wit = txn_data["vin"][1]["witness"]
+wit_asm = txn_data["vin"][1]["prevout"]["scriptpubkey_asm"]
+txn_data = segwit_txn_data(filename)
+
+print(f"p2wpkh::> {validate_p2wpkh_txn(wit, wit_asm, txn_data)}")
