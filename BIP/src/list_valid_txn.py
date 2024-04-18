803fc15b7b71284554db86b559713f82110cafca
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-04-18 21:47:16 +0530

    fix::> used for making list of valid txns

---
+++ BIP/src/list_valid_txn.py
@@ -0,0 +1,30 @@
+import os
+import validate_txn
+'''
+@title read transaction form mempool
+@notice parses each json object in `mempool` and append an internal list with all txn_ids
+@return list of transaction ids
+'''
+def read_transactions():
+    txn_ids = []
+    mempool_dir = "mempool"
+    try:
+        for filename in os.listdir(mempool_dir):
+            with open(os.path.join(mempool_dir, filename), "r") as file:
+                txn_ids.append(filename[:-5])
+        return txn_ids
+    except Exception as e:
+        print("Error:", e)
+        return None
+
+def list_valid_txn():
+    valid_txn_ids = []
+    unchecked_txn_ids = read_transactions()
+    for txn_id in unchecked_txn_ids:
+        if validate_txn.validate(txn_id):
+            valid_txn_ids.append(txn_id)
+    return valid_txn_ids
+
+
+print(list_valid_txn())
+print(len(list_valid_txn())) # p2pkh - 311
