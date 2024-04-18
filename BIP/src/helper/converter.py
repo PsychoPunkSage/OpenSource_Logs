2769b619165e13fa9d739312039aa64440a9db0c
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-04-18 23:55:41 +0530

    fix::> Updated helper func used in the project

---
+++ BIP/src/helper/converter.py
@@ -0,0 +1,26 @@
+import hashlib
+
+
+def to_compact_size(value):
+    if value < 0xfd:
+        return value.to_bytes(1, byteorder='little').hex()
+    elif value <= 0xffff:
+        return (0xfd).to_bytes(1, byteorder='little').hex() + value.to_bytes(2, byteorder='little').hex()
+    elif value <= 0xffffffff:
+        return (0xfe).to_bytes(1, byteorder='little').hex() + value.to_bytes(4, byteorder='little').hex()
+    else:
+        return (0xff).to_bytes(1, byteorder='little').hex() + value.to_bytes(8, byteorder='little').hex()
+
+def to_little_endian(num, size):
+    return num.to_bytes(size, byteorder='little').hex()
+
+def to_hash160(hex_input):
+    # print(hex_input)
+    sha = hashlib.sha256(bytes.fromhex(hex_input)).hexdigest()
+    hash_160 = hashlib.new('ripemd160')
+    hash_160.update(bytes.fromhex(sha))
+
+    return hash_160.hexdigest()
+
+def to_hash256(hex_input):
+    return hashlib.sha256(hashlib.sha256(bytes.fromhex(hex_input)).digest()).digest().hex()
