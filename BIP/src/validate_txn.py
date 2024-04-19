ede7488d508250321a4229e9c8fc44cd3d5422d8
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-04-19 19:33:47 +0530

    fix::> canged the implemetation + removed redundant func

--- BIP/src/validate_txn.py
+++ BIP/src/validate_txn.py
@@ -2,13 +2,14 @@ import os
 import json
 import hashlib
 import scripts.p2pkh
+import helper.get_txn_id as tx_id  
 
 ################
 ## Txn Weight ##
 ################
 def _txn_weight(txnId):
-    tx_bytes = len(_create_raw_txn_data(txnId))//2
-    tx_weight = 4*(len(_create_raw_txn_hash_wo_witness(txnId))//2) + (tx_bytes - len(_create_raw_txn_hash_wo_witness(txnId))//2)
+    tx_bytes = len(tx_id.create_raw_txn_data_full(txnId))//2
+    tx_weight = 4*(len(tx_id.create_raw_txn_data_min(txnId))//2) + (tx_bytes - len(tx_id.create_raw_txn_data_min(txnId))//2)
     tx_virtual_weight = tx_weight/4
 
     return [tx_bytes, tx_weight, tx_virtual_weight]
@@ -16,7 +17,7 @@ def _txn_weight(txnId):
 ##########
 ## FEES ##
 ##########
-def _fees(txnId):
+def fees(txnId):
     file_path = os.path.join('mempool', f'{txnId}.json') # file path
     if os.path.exists(file_path):
         # Read the JSON data from the file
@@ -31,103 +32,14 @@ def _fees(txnId):
 ##############
 ## Txn Data ##
 ##############
-def _create_raw_txn_data(txn_id):
-    txn_hash = ""
-
-    file_path = os.path.join("mempool", f"{txn_id}.json")
-    if os.path.exists(file_path):
-        with open(file_path, 'r') as f:
-            data = json.load(f)
-            # Version
-            txn_hash += f"{_little_endian(data['version'], 4)}"
-            # Marker+flags (if any `vin` has empty scriptsig)
-            if any(i.get("scriptsig") == "" for i in data["vin"]):
-                txn_hash += "0001"
-            # No. of inputs:
-            txn_hash += f"{str(_to_compact_size(len(data['vin'])))}"
-            # Inputs
-            for iN in data["vin"]:
-                txn_hash += f"{bytes.fromhex(iN['txid'])[::-1].hex()}"
-                txn_hash += f"{_little_endian(iN['vout'], 4)}"
-                txn_hash += f"{_to_compact_size(len(iN['scriptsig'])//2)}" # FLAG@> maybe not divided by 2
-                txn_hash += f"{iN['scriptsig']}"
-                txn_hash += f"{_little_endian(iN['sequence'], 4)}"
-
-            # No. of outputs
-            txn_hash += f"{str(_to_compact_size(len(data['vout'])))}"
-
-            # Outputs
-            for out in data["vout"]:
-                txn_hash += f"{_little_endian(out['value'], 8)}"
-                txn_hash += f"{_to_compact_size(len(out['scriptpubkey'])//2)}"
-                txn_hash += f"{out['scriptpubkey']}"
-
-            # witness
-            for i in data["vin"]:
-                if "witness" in i and i["witness"]:
-                    txn_hash += f"{_to_compact_size(len(i['witness']))}"
-                    for j in i["witness"]:
-                        txn_hash += f"{_to_compact_size(len(j) // 2)}"
-                        txn_hash += f"{j}"
-
-            # Locktime
-            txn_hash += f"{_little_endian(data['locktime'], 4)}"
-            # print(f"txn_hash: {txn_hash}")
-    return txn_hash
-
-def _create_raw_txn_hash_wo_witness(txn_id):
-    txn_hash = ""
-
-    file_path = os.path.join("mempool", f"{txn_id}.json")
-    if os.path.exists(file_path):
-        with open(file_path, 'r') as f:
-            data = json.load(f)
-            # print(f"data: {data}")
-            # Version
-            txn_hash += f"{_little_endian(data['version'], 4)}"
-            # No. of inputs:
-            txn_hash += f"{str(_to_compact_size(len(data['vin'])))}"
-            # Inputs
-            for iN in data["vin"]:
-                txn_hash += f"{bytes.fromhex(iN['txid'])[::-1].hex()}"
-                txn_hash += f"{_little_endian(iN['vout'], 4)}"
-                txn_hash += f"{_to_compact_size(len(iN['scriptsig'])//2)}" # FLAG@> maybe not divided by 2
-                txn_hash += f"{iN['scriptsig']}"
-                txn_hash += f"{_little_endian(iN['sequence'], 4)}"
-
-            # No. of outputs
-            txn_hash += f"{str(_to_compact_size(len(data['vout'])))}"
-
-            # Outputs
-            for out in data["vout"]:
-                txn_hash += f"{_little_endian(out['value'], 8)}"
-                txn_hash += f"{_to_compact_size(len(out['scriptpubkey'])//2)}"
-                txn_hash += f"{out['scriptpubkey']}"
-
-            # Locktime
-            txn_hash += f"{_little_endian(data['locktime'], 4)}"
-    return txn_hash
-
-def _to_compact_size(value):
-    if value < 0xfd:
-        return value.to_bytes(1, byteorder='little').hex()
-    elif value <= 0xffff:
-        return (0xfd).to_bytes(1, byteorder='little').hex() + value.to_bytes(2, byteorder='little').hex()
-    elif value <= 0xffffffff:
-        return (0xfe).to_bytes(1, byteorder='little').hex() + value.to_bytes(4, byteorder='little').hex()
-    else:
-        return (0xff).to_bytes(1, byteorder='little').hex() + value.to_bytes(8, byteorder='little').hex()
-
-def _little_endian(num, size):
-    return num.to_bytes(size, byteorder='little').hex()
 
 #################
 ## TxnId Check ##
 #################
 def _get_txn_id(txn_id):
-    txn_data = _create_raw_txn_data(txn_id) # get raw txn_data
+    txn_data = tx_id.create_raw_txn_data_full(txn_id) # get raw txn_data
     if txn_data[8:12] == "0001":
-        txn_data = _create_raw_txn_hash_wo_witness(txn_id)
+        txn_data = tx_id.create_raw_txn_data_min(txn_id)
     txn_hash = hashlib.sha256(hashlib.sha256(bytes.fromhex(txn_data)).digest()).digest().hex() # 2xSHA256
     reversed_bytes = bytes.fromhex(txn_hash)[::-1].hex() # bytes reversal
     txnId = hashlib.sha256(bytes.fromhex(reversed_bytes)).digest().hex() # last sha256
@@ -137,7 +49,7 @@ def _get_txn_id(txn_id):
 ## Segwit/Non-Segwit ##
 #######################
 def _is_segwit(txn_id):
-    txn_data = _create_raw_txn_data(txn_id) # get raw txn_data
+    txn_data = tx_id.create_raw_txn_data_full(txn_id) # get raw txn_data
     # print(txn_data) # print
     # print(txn_data[8:12])
     if txn_data[8:12] == "0001":
@@ -205,10 +117,10 @@ def validate(txnId):
 
     # return True
 
-# print("\nOUTPUT::>\n")
+print("\nOUTPUT::>\n")
 # print(validate("0a3c3139b32f021a35ac9a7bef4d59d4abba9ee0160910ac94b4bcefb294f196"))
 # print(validate("ff0717b6f0d2b2518cfb85eed7ccea44c3a3822e2a0ce6e753feecf68df94a7f"))
-# print(validate("0a8b21af1cfcc26774df1f513a72cd362a14f5a598ec39d915323078efb5a240")) # - p2pkh only
+print(validate("0a8b21af1cfcc26774df1f513a72cd362a14f5a598ec39d915323078efb5a240")) # - p2pkh only
 # print(validate("1ccd927e58ef5395ddef40eee347ded55d2e201034bc763bfb8a263d66b99e5e"))
 # print(validate("0a5d6ddc87a9246297c1038d873eec419f04301197d67b9854fa2679dbe3bd65"))
 
